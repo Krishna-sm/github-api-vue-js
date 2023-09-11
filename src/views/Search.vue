e8ecc544a0820e701e59bf2b7f44cd21c35625f8
Krishna-sm <krishna.web18@gmail.com>
2023-09-11 20:09:39 +0530

    complete

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -7,6 +7,9 @@ import {ref} from 'vue';
     const loading = ref(false);
     const datas = ref(null);
     const fetchedData = async()=>{
+        if(inputData.value<2){
+            return false;
+        }
         try {
             loading.value = true;
             const res = await axios.get(`https://api.github.com/users/${inputData.value}`);
@@ -23,8 +26,8 @@ import {ref} from 'vue';
 <template>
    <div class="flex justify-center flex-col items-center">
     
-               <form @submit.prevent="fetchedData" class="my-10 w-1/2 mx-auto bg-white flex items-center px-3 rounded-md">
-                <input v-model="inputData" type="text" placeholder="Search Any User" class="w-full h-10 py-10 text-gray-900 px-3 focus:outline-none focus:border-none text-2xl rounded-md ">
+               <form @submit.prevent="fetchedData" class="my-10 w-[95%] duration-300 transition-all md:w-1/2 mx-auto bg-white flex items-center px-3 rounded-md">
+                <input v-model="inputData" type="text" placeholder="Search Any User" class="w-full h-10 py-8 md:py-10 text-gray-900 px-3 focus:outline-none focus:border-none text-2xl rounded-md ">
                <button type="submit" >
                 <v-icon name="pr-search" scale="2" class="text-red-600" />
                </button>
@@ -32,7 +35,7 @@ import {ref} from 'vue';
    
                <v-icon v-if="loading" name="la-spinner-solid" class="text-3xl lg:text-6xl text-white animate-spin " scale="4"></v-icon>
 
-            <section v-if="datas || inputData.length>1" class="text-black mx-10 my-5 bg-white body-font">
+            <section v-if="datas && inputData.length>1" class="text-black mx-10 my-5 bg-white body-font">
   <div class="container mx-auto flex   md:flex-row flex-col items-center">
     <div class="lg:max-w-lg lg:w-full  md:w-1/2 w-5/6 mb-10 md:mb-0">
       <img class="  scale-110 object-cover object-center rounded-[3rem]" alt="hero" :src="datas?.avatar_url">
